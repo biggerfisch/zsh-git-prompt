0d5cea474ba98791b34a8139ad06fd9e256a0dde
Your Name <you@example.com>
2015-04-11 06:14:56 +0000

    First rewrite

--- gitstatus.py
+++ gitstatus.py
@@ -1,56 +1,59 @@
 #!/usr/bin/env python
 from __future__ import print_function
+from subprocess import Popen, PIPE
+import sys
 
 # change this symbol to whatever you prefer
 prehash = ':'
 
-from subprocess import Popen, PIPE
-
-import sys
-gitsym = Popen(['git', 'symbolic-ref', 'HEAD'], stdout=PIPE, stderr=PIPE)
-branch, error = gitsym.communicate()
+git_status_call = Popen(['git', 'status', '--porcelain', '--branch'], stdout=PIPE, stderr=PIPE)
+gitOutput, error = git_status_call.communicate()
 
 error_string = error.decode('utf-8')
 
-if 'fatal: Not a git repository' in error_string:
+if 'fatal' in error_string:
 	sys.exit(0)
 
-branch = branch.decode("utf-8").strip()[11:]
+# NEED TO FIX AFTER THIS LINE
 
-res, err = Popen(['git','diff','--name-status'], stdout=PIPE, stderr=PIPE).communicate()
-err_string = err.decode('utf-8')
-if 'fatal' in err_string:
-	sys.exit(0)
-changed_files = [namestat[0] for namestat in res.decode("utf-8").splitlines()]
-staged_files = [namestat[0] for namestat in Popen(['git','diff', '--staged','--name-status'], stdout=PIPE).communicate()[0].splitlines()]
-nb_changed = len(changed_files) - changed_files.count('U')
-nb_U = staged_files.count('U')
-nb_staged = len(staged_files) - nb_U
+#TODO parse gitOutput for this
+branch = Popen(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout=PIPE, stderr=PIPE).communicate()[0]
+
+splitOutput = gitOutput.replace(']', '').split('\n')
+
+stats = [line[:2] for line in splitOutput]
+
+nb_staged = 0
+nb_changed = 0
+nb_untracked = 0
+nb_conflicts = 0
+
+for entry in stats:
+	if entry == ' M':
+		nb_changed++
+	else if entry == 'M ':
+		nb_staged++
+	else if entry == '??':
+		nb_untracked++
+	else if entry == 'A ':
+		nb_staged++
+	else if entry == 'UU':
+		nb_conflicts++
+
+
 staged = str(nb_staged)
-conflicts = str(nb_U)
+conflicts = str(nb_conflicts)
 changed = str(nb_changed)
-nb_untracked = len([0 for status in Popen(['git','status','--porcelain',],stdout=PIPE).communicate()[0].decode("utf-8").splitlines() if status.startswith('??')])
 untracked = str(nb_untracked)
 
-ahead, behind = 0,0
+splitFirstLine = splitOutput[0].split()
 
-if not branch: # not on any branch
+ahead = splitFirstLine[splitFirstLine.index('[ahead')+1] if '[ahead' in splitFirstLine else 0
+behind = splitFirstLine[splitFirstLine.index('[behind')+1] if '[behind' in splitFirstLine else 0
+
+if branch == 'HEAD':
 	branch = prehash + Popen(['git','rev-parse','--short','HEAD'], stdout=PIPE).communicate()[0].decode("utf-8")[:-1]
-else:
-	remote_name = Popen(['git','config','branch.%s.remote' % branch], stdout=PIPE).communicate()[0].decode("utf-8").strip()
-	if remote_name:
-		merge_name = Popen(['git','config','branch.%s.merge' % branch], stdout=PIPE).communicate()[0].decode("utf-8").strip()
-		if remote_name == '.': # local
-			remote_ref = merge_name
-		else:
-			remote_ref = 'refs/remotes/%s/%s' % (remote_name, merge_name[11:])
-		revgit = Popen(['git', 'rev-list', '--left-right', '%s...HEAD' % remote_ref],stdout=PIPE, stderr=PIPE)
-		revlist = revgit.communicate()[0]
-		if revgit.poll(): # fallback to local
-			revlist = Popen(['git', 'rev-list', '--left-right', '%s...HEAD' % merge_name],stdout=PIPE, stderr=PIPE).communicate()[0]
-		behead = revlist.decode("utf-8").splitlines()
-		ahead = len([x for x in behead if x[0]=='>'])
-		behind = len(behead) - ahead
+
 
 out = ' '.join([
 	branch,
