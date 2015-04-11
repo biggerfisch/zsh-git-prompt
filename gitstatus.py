3d87dbfbf5fd95af785db6a6c94cb7e6b9620a03
Your Name <you@example.com>
2015-04-11 06:54:53 +0000

    Wrap everything in a function

--- gitstatus.py
+++ gitstatus.py
@@ -6,63 +6,66 @@ import sys
 # change this symbol to whatever you prefer
 prehash = ':'
 
-git_status_call = Popen(['git', 'status', '--porcelain', '--branch'], stdout=PIPE, stderr=PIPE)
-gitOutput, error = git_status_call.communicate()
+def getStatus():
+	git_status_call = Popen(['git', 'status', '--porcelain', '--branch'], stdout=PIPE, stderr=PIPE)
+	gitOutput, error = git_status_call.communicate()
 
-error_string = error.decode('utf-8')
+	error_string = error.decode('utf-8')
 
-if 'fatal' in error_string:
-	sys.exit(0)
-
-# NEED TO FIX AFTER THIS LINE
-
-#TODO parse gitOutput for this
-branch = Popen(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout=PIPE, stderr=PIPE).communicate()[0].decode("utf-8").replace('\n', '')
-
-splitOutput = gitOutput.replace(']', '').replace(',', '').split('\n')
-
-stats = [line[:2] for line in splitOutput]
-
-nb_staged = 0
-nb_changed = 0
-nb_untracked = 0
-nb_conflicts = 0
-
-for entry in stats:
-	if entry == ' M':
-		nb_changed += 1
-	elif entry == 'M ':
-		nb_staged += 1
-	elif entry == '??':
-		nb_untracked += 1
-	elif entry == 'A ':
-		nb_staged += 1
-	elif entry == 'UU':
-		nb_conflicts += 1
+	if 'fatal' in error_string:
+		return
 
 
-staged = str(nb_staged)
-conflicts = str(nb_conflicts)
-changed = str(nb_changed)
-untracked = str(nb_untracked)
+	#TODO parse gitOutput for this
+	branch = Popen(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout=PIPE, stderr=PIPE).communicate()[0].decode("utf-8").replace('\n', '')
 
-splitFirstLine = splitOutput[0].split()
+	splitOutput = gitOutput.replace(']', '').replace(',', '').split('\n')
 
-ahead = splitFirstLine[splitFirstLine.index('[ahead')+1] if '[ahead' in splitFirstLine else 0
-behind = splitFirstLine[splitFirstLine.index('[behind')+1] if '[behind' in splitFirstLine else 0
+	stats = [line[:2] for line in splitOutput]
 
-if branch == 'HEAD':
-	branch = prehash + Popen(['git','rev-parse','--short','HEAD'], stdout=PIPE).communicate()[0].decode("utf-8")[:-1]
+	nb_staged = 0
+	nb_changed = 0
+	nb_untracked = 0
+	nb_conflicts = 0
+
+	for entry in stats:
+		if entry == ' M':
+			nb_changed += 1
+		elif entry == 'M ':
+			nb_staged += 1
+		elif entry == '??':
+			nb_untracked += 1
+		elif entry == 'A ':
+			nb_staged += 1
+		elif entry == 'UU':
+			nb_conflicts += 1
 
 
-out = ' '.join([
-	branch,
-	str(ahead),
-	str(behind),
-	staged,
-	conflicts,
-	changed,
-	untracked,
-	])
-print(out, end='')
+	staged = str(nb_staged)
+	conflicts = str(nb_conflicts)
+	changed = str(nb_changed)
+	untracked = str(nb_untracked)
 
+	splitFirstLine = splitOutput[0].split()
+
+	ahead = splitFirstLine[splitFirstLine.index('[ahead')+1] if '[ahead' in splitFirstLine else 0
+	behind = splitFirstLine[splitFirstLine.index('[behind')+1] if '[behind' in splitFirstLine else 0
+
+	if branch == 'HEAD':
+		branch = prehash + Popen(['git','rev-parse','--short','HEAD'], stdout=PIPE).communicate()[0].decode("utf-8")[:-1]
+
+
+	out = ' '.join([
+		branch,
+		str(ahead),
+		str(behind),
+		staged,
+		conflicts,
+		changed,
+		untracked,
+		])
+	print(out, end='')
+
+
+if __name__ == "__main__":
+	getStatus()
